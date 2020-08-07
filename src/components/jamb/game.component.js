78c43b8df7aa04a050688cd858b20b4293812eb5
David Wang <devtech324@gmail.com>
2020-08-07 22:59:09 +0200

    deleted reduntant files and fixed formId bug

--- src/components/jamb/game.component.js
+++ src/components/jamb/game.component.js
@@ -326,7 +326,7 @@ export default class Game extends Component {
     restart() {
         if (this.props.user) {
             var user = this.props.user;
-            var url = this.state.apiURL + 'forms/' + formId;
+            var url = this.state.apiURL + 'forms/' + this.state.formId;
             var http = new XMLHttpRequest();
             http.open('DELETE', url, true);
             http.setRequestHeader('Content-type', 'application/json');
